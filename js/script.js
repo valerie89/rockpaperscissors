bc9e914921d91055ded8920cd6cce1ef74938c4a
Glitch (sky-maze-meeting) <none>
2021-11-16 23:28:04 +0000

    rock paper scissors project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+  $(".play").click(function(){
+  let userInput = $ (".input").val();
+  $(".userChoice").text(userInput);
 
+    let randomNum= 0;
+randomNum = Math.random();
+
+if (randomNum <= (1/3)){
+$(".computerChoice").text("paper");
+}
+
+else if (randomNum <= (2/3)){
+  $(".computerChoice").text("rock");
+}
+else {
+  $(".computerChoice").text("scissors");
+  };
+})
+
+
+
+  
