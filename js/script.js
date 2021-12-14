5a76de8fa2eafe0368d581438ce9aec5bc3a2a36
Glitch (sky-maze-meeting) <none>
2021-12-14 23:19:09 +0000

    🎿💈 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@
 
     let randomNum= 0;
 randomNum = Math.random();
+    let computerChoice= 0;
+    
+if (randomNum <= (1/3)) {
+	computerChoice = "paper";
+} else if (randomNum <= (2/3)) {
+	computerChoice = "rock";
+} else {
+	computerChoice = "scissors";
+}
 
+$(".computerChoice").text(computerChoice);
+
+    
+    
 if (randomNum <= (1/3)){
 $(".computerChoice").text("paper");
 }
@@ -14,9 +27,8 @@ else if (randomNum <= (2/3)){
 }
 else {
   $(".computerChoice").text("scissors");
-  };
-})
-
-
-
   
+  if (userInput === 'paper'&& computerChoice === 'paper'){
+    $(".result").html("tie!");
+    
+    else if 
